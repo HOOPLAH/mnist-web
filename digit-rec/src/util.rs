d0cc79d613a10ee788c7338f7c0ae9f17c52b4ad
Theodore DeRego <tderego94@gmail.com>
2016-04-04 08:28:55 -1000

    Added util functions to read/write Graph variables to/from files

--- digit-rec/src/util.rs
+++ digit-rec/src/util.rs
@@ -1,9 +1,40 @@
-use std::io::{Read, Write};
+use std::fs::File;
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::path::Path;
+
+use deeplearn::{Graph, VarIndex};
 use ga::Array;
 
 use msgpack::{encode, decode};
 
-fn read_array<T: Read>(reader: &mut T) -> Result<Array<f32>, decode::ValueReadError> {
+fn read_var_from_file<P: AsRef<Path>>(path: P, graph: &Graph, v: VarIndex)
+                                      -> Result<(), decode::ValueReadError> {
+    let ref mut file = BufReader::new(File::open(path).unwrap());
+    try!(read_var(file, graph, v));
+    Ok(())
+}
+
+fn read_var<R: Read>(r: &mut R, graph: &Graph, v: VarIndex)
+                     -> Result<(), decode::ValueReadError> {
+    let array = try!(read_array(r));
+    v.get(graph).set(graph.context(), &array);
+    Ok(())
+}
+
+fn write_var_to_file<P: AsRef<Path>>(path: P, graph: &Graph, v: VarIndex)
+                                     -> Result<(), encode::ValueWriteError> {
+    let ref mut file = BufWriter::new(File::open(path).unwrap());
+    try!(write_var(file, graph, v));
+    Ok(())
+}
+
+fn write_var<W: Write>(w: &mut W, graph: &Graph, v: VarIndex)
+                       -> Result<(), encode::ValueWriteError> {
+    try!(write_array(w, v.get_cpu(graph)));
+    Ok(())
+}
+
+fn read_array<R: Read>(reader: &mut R) -> Result<Array<f32>, decode::ValueReadError> {
     // Read the shape
     let dim_count = try!(decode::read_array_size(reader));
     let mut shape: Vec<usize> = vec![];
@@ -23,7 +54,7 @@ fn read_array<T: Read>(reader: &mut T) -> Result<Array<f32>, decode::ValueReadEr
     Ok(Array::from_vec(shape, buf))
 }
 
-fn write_array<T: Write>(writer: &mut T, array: Array<f32>) -> Result<(), encode::ValueWriteError> {
+fn write_array<W: Write>(writer: &mut W, array: Array<f32>) -> Result<(), encode::ValueWriteError> {
     try!(encode::write_array_len(writer, array.shape().len() as u32)); // Write the dimension count
 
     // Write all of the dimensions
